4608537706ef3ee6e7c63c973ccb3c7ec8aa0bb5
Bill Willits <willitsw@gmail.com>
2023-02-06 22:35:16 -0500

    add csrf trusted origin config

--- backend/whatalesyou/settings.py
+++ backend/whatalesyou/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+CSRF_TRUSTED_ORIGINS = ["https://what-ales-you.herokuapp.com"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
