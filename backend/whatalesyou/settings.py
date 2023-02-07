d896924cf0bc8e03d2989758ad490a231f4ac007
Bill Willits <willitsw@gmail.com>
2023-02-06 20:30:43 -0500

    heroku first stab

--- backend/whatalesyou/settings.py
+++ backend/whatalesyou/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://what-ales-you.herokuapp.com/"]
 
 
 # Application definition
