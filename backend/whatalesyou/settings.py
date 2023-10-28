144951378336eac930d584502cbdee1a6e5918a3
Bill Willits <willitsw@gmail.com>
2023-10-28 11:51:35 -0400

    add dev frontend to cors another try

--- backend/whatalesyou/settings.py
+++ backend/whatalesyou/settings.py
@@ -36,13 +36,12 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "what-ales-you.herokuapp.com",
     "https://what-ales-you.herokuapp.com",
-    "https://enchanting-starlight-bc28a8.netlify.app/",
-    "https://enchanting-starlight-bc28a8.netlify.app",
-    "enchanting-starlight-bc28a8.netlify.app",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
+    "https://enchanting-starlight-bc28a8.netlify.app/",
+    "https://whatalesyou.net/",
 ]
 
 # Application definition
