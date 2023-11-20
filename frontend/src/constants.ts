129cd93eb4f41f7629c2929496bca6ae6c0f3502
Bill Willits <willitsw@gmail.com>
2023-11-19 20:02:40 -0500

    use https prod

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -12,7 +12,7 @@ let constants: Constants = {
 };
 
 if (constants.environment === "production") {
-  constants.apiUrl = "https://api.whatalesyou.net";
+  constants.apiUrl = "http://api.whatalesyou.net";
 }
 
 export const DATE_FORMAT = "MM/DD/YYYY";
