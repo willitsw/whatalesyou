3af85e82478cf8a35f75cba068545f0e44e1383b
Bill Willits <willitsw@gmail.com>
2023-12-01 16:39:31 -0500

    clean up some comments

--- frontend/src/utils/api-calls.ts
+++ frontend/src/utils/api-calls.ts
@@ -12,15 +12,6 @@ import {
 } from "../types/user";
 import makeRequest from "./request";
 
-// RECIPE ENDPOINTS
-
-// type RecipeApiType = {
-//   getByUser: (userId: string) => Promise<UseQueryResult<RecipeDetailed[]>>;
-//   getById: (recipeId: string) => Promise<RecipeDetailed>;
-//   delete: (recipeId: string) => Promise<void>;
-//   createUpdate: (recipe: Recipe) => Promise<void>;
-// };
-
 export const useGetRecipesByUser = () =>
   useQuery<RecipeListResponse>({
     queryKey: ["recipe", "list"],
@@ -68,46 +59,6 @@ export const useCreateUpdateRecipe = () => {
   });
 };
 
-// export const RecipeApi: RecipeApiType = {
-//   getByUser: async (userId: string) =>
-//     useQuery({
-//       queryKey: ["recipesByUser", userId],
-//       queryFn: (await makeRequest("/recipes/", "GET")).body,
-//     }),
-//   getById: async (recipeId: string) =>
-//     (await makeRequest(`/recipes/${recipeId}/`, "GET")).body,
-//   delete: async (recipeId: string) =>
-//     (await makeRequest(`/recipes/${recipeId}/`, "DELETE")).body,
-//   createUpdate: async (recipe: Recipe) =>
-//     (await makeRequest(`/recipes/${recipe.id}/`, "PUT", recipe)).body,
-// };
-
-// export const getRecipesByUser = async (): Promise<RecipeListResponse> => {
-//   return (await makeRequest("/recipes/", "GET")).body;
-// };
-
-// export const getRecipeById = async (
-//   recipeId: string
-// ): Promise<RecipeDetailed> => {
-//   return (await makeRequest(`/recipes/${recipeId}/`, "GET")).body;
-// };
-
-// export const deleteRecipe = async (recipeId: string): Promise<void> => {
-//   return (await makeRequest(`/recipes/${recipeId}/`, "DELETE")).body;
-// };
-
-// export const createUpdateRecipe = async (recipe: Recipe): Promise<Recipe> => {
-//   return (await makeRequest(`/recipes/${recipe.id}/`, "PUT", recipe)).body;
-// };
-
-// BREW SETTINGS ENDPOINTS
-
-// export const useGetBrewSettings = () =>
-//   useQuery<BrewSettings>({
-//     queryKey: ["brewSettings"],
-//     queryFn: async () => (await makeRequest("/brew-settings/", "GET")).body,
-//   });
-
 export const useCreateUpdateBrewSettings = () => {
   const queryClient = useQueryClient();
   return useMutation<void, Error, BrewSettings>({
@@ -124,18 +75,6 @@ export const useCreateUpdateBrewSettings = () => {
     },
   });
 };
-
-// export const getBrewSettings = async (): Promise<BrewSettings> => {
-//   return (await makeRequest("/users/", "GET")).body;
-// };
-
-// export const createUpdateBrewSettings = async (
-//   brewSettings: BrewSettings
-// ): Promise<BrewSettings> => {
-//   return (
-//     await makeRequest(`/brew-settings/${brewSettings.id}/`, "PUT", brewSettings)
-//   ).body;
-// };
 
 // BREW LOG ENDPOINTS
 
@@ -189,24 +128,6 @@ export const useDeleteBrewLog = (brewLogId: string) => {
   });
 };
 
-// export const getBrewLogsByUser = async (): Promise<BrewLogListResponse> => {
-//   return (await makeRequest("/brew-logs/", "GET")).body;
-// };
-
-// export const getBrewLogById = async (brewLogId: string): Promise<BrewLog> => {
-//   return (await makeRequest(`/brew-logs/${brewLogId}/`, "GET")).body;
-// };
-
-// export const deleteBrewLog = async (brewLogId: string): Promise<void> => {
-//   return (await makeRequest(`/brew-logs/${brewLogId}/`, "DELETE")).body;
-// };
-
-// export const createUpdateBrewLog = async (
-//   brewLog: BrewLog
-// ): Promise<BrewLog> => {
-//   return (await makeRequest(`/brew-logs/${brewLog.id}/`, "PUT", brewLog)).body;
-// };
-
 // TOKEN ENDPOINTS
 
 export const getToken = async (body: TokenRequest): Promise<Response> => {
@@ -235,10 +156,6 @@ export const refreshToken = async (): Promise<void> => {
 };
 
 // USER ENDPOINTS
-
-// export const getCurrentUser = async (id: string): Promise<UserResponse> => {
-//   return (await makeRequest(`/users/${id}/`, "GET")).body;
-// };
 
 export const getCurrentUser = async (id: string): Promise<UserResponse> => {
   const result = await makeRequest(`/users/${id}/`, "GET");
@@ -281,15 +198,6 @@ export const useUpdateUser = () => {
     },
   });
 };
-
-// export const createUser = async (body: UserRequest): Promise<UserResponse> => {
-//   const result = await makeRequest("/users/", "POST", body, { useAuth: false });
-//   return { code: result.code, ...result.body };
-// };
-
-// export const updateUser = async (body: User): Promise<UserResponse> => {
-//   return (await makeRequest(`/users/${body.id}/`, "PUT", body)).body;
-// };
 
 export const validateEmailToken = async (
   body: EmailValidationRequest
