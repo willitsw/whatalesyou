c25d3547e6573fba33a1db42c817c2b88f3ca7f7
Bill Willits <willitsw@gmail.com>
2023-12-08 15:46:21 -0500

    try to get all routes working

--- whatalesyou/settings.py
+++ whatalesyou/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import mimetypes
 import os
 from datetime import timedelta
 from pathlib import Path
+
+mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
