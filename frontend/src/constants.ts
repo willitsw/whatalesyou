f75c1e2a94c91d551a02bf8f39329fa6f7c56ec0
Bill Willits <willitsw@gmail.com>
2023-12-14 16:12:55 -0500

    build test assets in ci

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,18 +1,24 @@
-type EnvironmentType = "production" | "development";
+type EnvironmentType = "production" | "development" | "testing";
 interface Constants {
   apiUrl: string;
   environment: EnvironmentType;
   enableAnalytics: boolean;
+  cypressBaseUrl: string;
 }
 
 let constants: Constants = {
   apiUrl: "http://localhost:8000/api",
   environment: process.env.APP_ENV as EnvironmentType,
   enableAnalytics: false,
+  cypressBaseUrl: "http://localhost:8081",
 };
 
 if (constants.environment === "production") {
   constants.apiUrl = "https://what-ales-you.herokuapp.com/api";
+}
+
+if (constants.environment === "testing") {
+  constants.cypressBaseUrl = "http://localhost:8080";
 }
 
 export const DATE_FORMAT = "MM/DD/YYYY";
