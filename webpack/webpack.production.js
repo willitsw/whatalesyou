ff9514c0bb693cc9f9d8cf990a448ebceeb99021
Bill Willits <willitsw@gmail.com>
2023-12-14 14:58:36 -0500

    run cypress in action

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -15,7 +15,7 @@ module.exports = merge(sharedConfig, {
   mode: "production",
   plugins: [
     new webpack.EnvironmentPlugin({
-      APP_ENV: "production",
+      APP_ENV: "testing",
     }),
   ],
 });
