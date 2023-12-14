dfc213d66deea527339404a5ec21b01334cdf7ef
Bill Willits <willitsw@gmail.com>
2023-12-14 15:46:27 -0500

    build test assets in ci

---
+++ webpack/webpack.testing.js
@@ -0,0 +1,21 @@
+const { merge } = require("webpack-merge");
+const sharedConfig = require("./webpack.shared.js");
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = merge(sharedConfig, {
+  entry: {
+    frontend: "./frontend/src/index.tsx",
+  },
+  output: {
+    path: path.resolve("./frontend/static/frontend/"),
+    filename: "[name]-[hash].js",
+    publicPath: "static/frontend/",
+  },
+  mode: "production",
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      APP_ENV: "testing",
+    }),
+  ],
+});
